43746ad7bc46a6fca338a3dc06f5803045a9cba3
Peter Caulfield <peterjcaulfield@gmail.com>
2014-10-05 19:45:34 +0100

    updated bindings

--- .mjolnir/init.lua
+++ .mjolnir/init.lua
@@ -10,7 +10,8 @@ local fnutils = require "mjolnir.fnutils"
 local grid = require "mjolnir.bg.grid"
 
 local mash = {"alt", "cmd"}
-local mashshift = {"alt", "cmd", "shift"}
+local mash2 = {"ctrl", "cmd"}
+local mash3 = {"ctrl", "alt", "cmd"}
 
 -- change default grid dimensions (needs to have diff defaults depending on screen res eventually)
 grid.GRIDHEIGHT = 4
@@ -25,10 +26,10 @@ hotkey.bind(mash, "'", function() fnutils.map(window.visiblewindows(), grid.snap
 hotkey.bind(mash, '=', function() grid.adjustwidth( 1) end)
 hotkey.bind(mash, '-', function() grid.adjustwidth(-1) end)
 
-hotkey.bind(mashshift, 'H', function() window.focusedwindow():focuswindow_west() end)
-hotkey.bind(mashshift, 'L', function() window.focusedwindow():focuswindow_east() end)
-hotkey.bind(mashshift, 'K', function() window.focusedwindow():focuswindow_north() end)
-hotkey.bind(mashshift, 'J', function() window.focusedwindow():focuswindow_south() end)
+hotkey.bind(mash3, 'H', function() window.focusedwindow():focuswindow_west() end)
+hotkey.bind(mash3, 'L', function() window.focusedwindow():focuswindow_east() end)
+hotkey.bind(mash3, 'K', function() window.focusedwindow():focuswindow_north() end)
+hotkey.bind(mash3, 'J', function() window.focusedwindow():focuswindow_south() end)
 
 hotkey.bind(mash, 'M', grid.maximize_window)
 
@@ -40,7 +41,7 @@ hotkey.bind(mash, 'K', grid.pushwindow_up)
 hotkey.bind(mash, 'H', grid.pushwindow_left)
 hotkey.bind(mash, 'L', grid.pushwindow_right)
 
-hotkey.bind(mash, 'U', grid.resizewindow_thinner)
-hotkey.bind(mash, 'I', grid.resizewindow_taller)
-hotkey.bind(mash, 'O', grid.resizewindow_shorter)
-hotkey.bind(mash, 'P', grid.resizewindow_wider)
+hotkey.bind(mash2, 'H', grid.resizewindow_thinner)
+hotkey.bind(mash2, 'J', grid.resizewindow_taller)
+hotkey.bind(mash2, 'K', grid.resizewindow_shorter)
+hotkey.bind(mash2, 'L', grid.resizewindow_wider)
